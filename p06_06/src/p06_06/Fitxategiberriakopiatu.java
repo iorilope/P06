401c6018e19732a2352aa93863396b7448956e8e
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-12-13 09:02:20 +0100

    bi modu

---
+++ p06_06/src/p06_06/Fitxategiberriakopiatu.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package p06_06;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ioritz Lopetegi
+ */
+public class Fitxategiberriakopiatu {
+	 public static void main(String[] args) throws Exception {
+		 
+		 Scanner teklatua = new Scanner(System.in);
+
+			System.out.println("Sartu Fitxategiaren helbide absolutua");
+			String fitxategiZaharra = teklatua.nextLine();
+
+			System.out.println("Nora mugitu nahi dituzu datuak (Direktorioa):");
+
+			String	DirLekuBerria = teklatua.nextLine();
+	        FileInputStream inputStream = new FileInputStream(fitxategiZaharra);
+	        FileOutputStream outputStream = new FileOutputStream(DirLekuBerria);
+
+	        byte[] buffer = new byte[1024];
+	        int bytesRead;
+
+	        while ((bytesRead = inputStream.read(buffer)) != -1) {
+	            outputStream.write(buffer, 0, bytesRead);
+	        }
+
+	        inputStream.close();
+	        outputStream.close();
+	    }
+	}
+
